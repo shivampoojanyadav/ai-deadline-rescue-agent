91562410a73dc55f94dcca59f6224523dde5f32c
shivampoojanyadav <yshivampoojan@gmail.com>
2026-06-30 14:41:30 +0530

    configure project for render deployment

--- deadline_rescue/settings.py
+++ deadline_rescue/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-*#!ltd51)_wbzg-n0wlijkx=hjv$*wgmbu+wc%0m550a_1-4ut
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 #added manually 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
